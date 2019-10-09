a42ebf10f9e485a1744e9796d61ce954673f4abf
Alex Tomberg <a.tomberg@gmail.com>
2019-10-08 20:59:22 -0400

    Added tests and fixed stuff

--- src/lib.rs
+++ src/lib.rs
@@ -39,11 +39,11 @@ impl Serialize for f64 {
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct SparseRecord<F: Float + Serialize> {
     pub row: usize,
     pub col: usize,
-    pub cooc: F,
+    pub val: F,
 }
 
 impl<F: Float + Serialize> SparseRecord<F> {
@@ -51,7 +51,7 @@ impl<F: Float + Serialize> SparseRecord<F> {
         let mut res = Vec::<u8>::new();
         res.extend(&self.row.to_le_bytes());
         res.extend(&self.col.to_le_bytes());
-        res.extend(&self.cooc.to_bytes());
+        res.extend(&self.val.to_bytes());
         res
     }
 
@@ -67,7 +67,7 @@ impl<F: Float + Serialize> SparseRecord<F> {
             Some(Self {
                 row: usize::from_le_bytes(w1_bytes),
                 col: usize::from_le_bytes(w2_bytes),
-                cooc: F::from_bytes(bytes[16..16 + F::BYTE_SIZE].to_vec()),
+                val: F::from_bytes(bytes[16..16 + F::BYTE_SIZE].to_vec()),
             })
         }
     }
@@ -113,7 +113,7 @@ impl<F: Float + Serialize> Ord for SparseRecord<F> {
 /// If `max_prod = 10`, we will pack all of the elements where row x col <= 10 into
 /// a single data vector, using the `row_offset` to quickly access any stored element.
 /// ```
-///  row_offset = [0         | 5          | 10     | 13   | 15   ]
+///  row_offset = [0         | 5          | 10     | 13   | 15   | 17]
 ///        data = [1 2 3 4 5 | 2 4 6 8 10 |  3 6 9 |  4 8 |  5 10]
 /// ```
 /// The remaining elements with products 12, 15, 16, 20 and 25 will be stored separately.
@@ -134,11 +134,13 @@ impl<F: Float + Serialize> CornerMatrix<F> {
             last += min(max_prod / i, max_size);
             row_offset.push(last);
         }
+        last += min(max_prod / max_size, max_size);
+        row_offset.push(last);
         CornerMatrix {
             max_size,
             max_prod,
             row_offset,
-            data: vec![F::zero(); last + min(max_prod / max_size, max_size)],
+            data: vec![F::zero(); last],
         }
     }
 
@@ -153,10 +155,10 @@ impl<F: Float + Serialize> CornerMatrix<F> {
     pub fn to_sparse(&self) -> Vec<SparseRecord<F>> {
         let mut result: Vec<SparseRecord<F>> = Vec::with_capacity(self.data.len());
         for row in 0..self.max_size {
-            for col in 0..(self.row_offset[row] - self.row_offset[row - 1]) {
-                let cooc = self.data[self.row_offset[row] + col];
-                if cooc != F::zero() {
-                    result.push(SparseRecord { row, col, cooc });
+            for col in 0..(self.row_offset[row + 1].saturating_sub(self.row_offset[row])) {
+                let val = self.data[self.row_offset[row] + col];
+                if val != F::zero() {
+                    result.push(SparseRecord { row, col, val });
                 }
             }
         }
@@ -208,13 +210,13 @@ impl<T: Copy> SymmetricCornerMatrix<T> {
 }
 
 /// This function
-pub fn estimate_max_prod(limit: f32, tolerance: f32) -> usize {
-    let mut n: f32 = 1e5;
-    let c: f32 = 0.154_431_33;
+pub fn estimate_max_prod<F: Float>(limit: F, tolerance: F) -> usize {
+    let mut n: F = F::from(1e5).unwrap();
+    let c: F = F::from(0.154_431_33).unwrap();
     while (limit - n * (n.ln() + c)).abs() > tolerance {
         n = limit / (n.ln() + c);
     }
-    n as usize
+    n.to_usize().unwrap()
 }
 
 #[cfg(test)]
@@ -230,7 +232,7 @@ mod tests {
     #[test]
     fn test_matrix_creation() {
         let matrix = CornerMatrix::<f32>::new(5, 10);
-        assert_eq!(matrix.row_offset, [0, 5, 10, 13, 15]);
+        assert_eq!(matrix.row_offset, [0, 5, 10, 13, 15, 17]);
         assert_eq!(matrix.data.len(), 17);
     }
 
@@ -240,5 +242,26 @@ mod tests {
         assert!((*matrix.get(2, 3) - 0.0).abs() < 0.1);
         matrix.set(2, 3, 5.0);
         assert!((*matrix.get(2, 3) - 5.0).abs() < 0.1);
+    }
+
+    #[test]
+    fn test_to_sparse() {
+        let mut matrix = CornerMatrix::<f32>::new(5, 10);
+        *matrix.get(2, 1) += 1.0;
+
+        let repr = matrix.to_sparse();
+        println!("{:?}", matrix);
+        println!("{:?}", repr);
+        assert!(repr.len() == 1);
+        assert!(
+            repr == vec![
+                SparseRecord::<f32> {
+                    row: 2,
+                    col: 1,
+                    val: 1.0
+                };
+                1
+            ]
+        );
     }
 }
